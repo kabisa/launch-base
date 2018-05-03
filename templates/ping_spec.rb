b2670e5cc53892b1440e129baa85a266d8025388
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-05-03 11:41:23 +0200

    Run the dummy app's specs

--- templates/ping_spec.rb
+++ templates/ping_spec.rb
@@ -1,7 +1,8 @@
 describe '/ping' do
-  it "renders 'pong'" do
+  it 'renders "pong"' do
     get '/ping'
-    expect(response).to be_success
+
+    expect(response).to be_successful
     expect(response.body).to eq 'pong'
   end
 end
