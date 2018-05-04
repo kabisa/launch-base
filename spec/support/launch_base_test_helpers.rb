3ebab0c42bc94c5f0597a6865fdb8085c7a748ec
nickve28 <nickvaneyk@gmail.com>
2018-05-04 14:36:32 +0200

    empty should be empty?

--- spec/support/launch_base_test_helpers.rb
+++ spec/support/launch_base_test_helpers.rb
@@ -3,7 +3,7 @@ module LaunchBaseTestHelpers
     environment_variable = 'DUMMY_APP_PATH'
     dummy_app_path = ENV[environment_variable]
 
-    raise "#{environment_variable} is missing" if (dummy_app_path || '').empty
+    raise "#{environment_variable} is missing" if (dummy_app_path || '').empty?
     raise "#{environment_variable} #{dummy_app_path} does not exist" unless File.directory?(dummy_app_path)
 
     dummy_app_path
