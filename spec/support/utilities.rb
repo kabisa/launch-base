bd8be8f4e7eb8a8b15071397471f7b67b73bfe6e
nickve28 <nickvaneyk@gmail.com>
2018-05-04 14:08:30 +0200

    Extract methods in capture helper

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,19 +2,32 @@
 def capture(stream)
   begin
     stream = stream.to_s
-    eval <<-RUBY, binding, __FILE__, __LINE__ + 1
-      $#{stream} = StringIO.new
-    RUBY
+
+    create_stream(stream)
     yield
-    result = eval <<-RUBY, binding, __FILE__, __LINE__ + 1
-      $#{stream}
-    RUBY
-    result = result.string
+    result = read_stream(stream)
   ensure
-    eval <<-RUBY, binding, __FILE__, __LINE__ + 1
-      $#{stream} = #{stream.upcase}
-    RUBY
+    stream_to_upcase(stream)
   end
 
   result
+end
+
+def create_stream(stream)
+  eval <<-RUBY, binding, __FILE__, __LINE__ + 1
+    $#{stream} = StringIO.new
+  RUBY
+end
+
+def read_stream(stream)
+  result = eval <<-RUBY, binding, __FILE__, __LINE__ + 1
+    $#{stream}
+  RUBY
+  result.string
+end
+
+def stream_to_upcase(stream)
+  eval <<-RUBY, binding, __FILE__, __LINE__ + 1
+    $#{stream} = #{stream.upcase}
+  RUBY
 end
