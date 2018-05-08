b47580e48eb3d1ccf955060013a0168e387ba889
Martijn Versluis <martijnversluis@users.noreply.github.com>
2018-05-08 13:04:48 +0200

    Prove the working transactions by testing twice

--- spec/fixtures/spec/features/kabisians_spec_.rb
+++ spec/fixtures/spec/features/kabisians_spec_.rb
@@ -1,10 +1,12 @@
 describe 'Kabisians' do
-  it 'is visible on the kabisians page', :js do
-    Kabisian.create!(name: 'Kamili')
+  2.times do
+    it 'is visible on the kabisians page', :js do
+      Kabisian.create!(name: 'Kamili')
 
-    visit '/kabisians'
+      visit '/kabisians'
 
-    expect(page).to have_content 'Hello, my name is Kamili'
-    expect(page).to have_content 'I have 0 colleagues'
+      expect(page).to have_content 'Hello, my name is Kamili'
+      expect(page).to have_content 'I have 0 colleagues'
+    end
   end
 end
